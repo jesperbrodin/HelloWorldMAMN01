3f55122ad81a3a55b3b7f520d6a98baaf3471225
jesper <brodinjesper@gmail.com>
2019-03-27 17:47:56 +0100

    First try at compass

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.helloworld;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button1 = (Button) findViewById(R.id.button);
+        ((View) button1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCompass();
+            }
+        });
+    }
+
+    public void openCompass() {
+        Intent intent = new Intent(this, Compass.class);
+        startActivity(intent);
     }
 }
